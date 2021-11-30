de4230c97ee13de10c0dba918770aca06d03871d
zahur <zahurmeerun@hotmail.com>
2021-11-30 21:31:16 +0400

    add build

--- src/features/allCharacters/allCharacters.js
+++ src/features/allCharacters/allCharacters.js
@@ -6,6 +6,7 @@ import SearchBar from "../searchTerm/searchTerm";
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Button from 'react-bootstrap/Button'
+import { setSearchTerm } from "../searchTerm/searchTermSlice";
 
 
 function AllCharacters() {
@@ -23,20 +24,18 @@ function AllCharacters() {
     allData.map(element=>{
       if(((element.name).toLowerCase()).includes(term.toLowerCase())){
         newList.push(element)
-        
-        console.log(newList)
       }           
     })
-    setData(newList)   
-  }
+    setData(newList)    
+  }  
 
   const renderCharacters = () => {
     return (data ).map((element)=>{
-      return    <Col xs={6} md={4} lg={3} className="text-light p-4">
+      return    <Col xs={6} sm={4} md={3} lg={3} className="text-light p-4">
                     <img src={element.image} alt={element.name} />                    
                     <div className="info">{element.name}</div>
                     <div className="info">{element.gender}</div>
-                    <div className="info">Birth Planet: {element.birth_planet}</div>
+                    <div className="info">Planet: {element.birth_planet}</div>
                     <div className="info pb-2">Birth Year: {element.birth_year}</div>                    
                 </Col>                
               
@@ -44,7 +43,7 @@ function AllCharacters() {
   }
   return (
     <div className="App">
-      <Button className="bg-dark text-warning rounded-0" onClick={myFunction}>click</Button>
+      <Button className="bg-dark text-warning rounded-0 m-2" onClick={myFunction}>click</Button>      
       <Row>
         {renderCharacters()}
       </Row>      
